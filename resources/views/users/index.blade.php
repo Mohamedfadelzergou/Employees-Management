3324a91b3ec5dc30c19bffea193ca36607f5bac2
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-08-23 23:48:15 +0100

    ajouter request (storage/update user) and add page crud user

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+@section('content')
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Users</h1>
+</div>
+<div class="row">
+   <div class="card mx-auto">
+    @if (session()->has('message'))
+    <div class="alert alert-success mb-2" role="alert">
+      {{session('message')}}
+    </div>
+    @endif
+       <div class="card-header">
+           <a href="{{route('users.create')}}" class="float-right">Create</a>
+       </div>
+       <div class="card-body">
+           <table class="table table-striped">
+               <thead>
+                 <tr>
+                   <th scope="col">#Id </th>
+                   <th scope="col">Username</th>
+                   <th scope="col">Email</th>
+                   <th scope="col">Manage</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 @foreach ($users as $user)    
+                 <tr>
+                   <th scope="row">{{$user->id}}</th>
+                   <td>{{$user->username}}</td>
+                   <td>{{$user->email}}</td>
+                   <td>
+                    <a href="{{route('users.edit',$user->id)}}" class="btn btn-success">Edit</a>
+                    
+                  </td>
+                 </tr>
+                 @endforeach
+               </tbody>
+             </table>
+       </div>
+   </div>
+</div>
+@endsection
